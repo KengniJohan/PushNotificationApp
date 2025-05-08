3a782d6fac07459a38ca3a866324d760e576f8d5
KengniJohan <kengnijordan9@gmail.com>
2025-05-08 15:25:53 +0100

    Init implementation

--- lib/main.dart
+++ lib/main.dart
@@ -1,51 +1,38 @@
 import 'package:flutter/material.dart';
+import "package:flutter_local_notifications/flutter_local_notifications.dart";
+// import 'package:flutter_native_timezone/flutter_native_timezone.dart';
+// import 'package:timezone/timezone.dart' as tz;
+// import 'package:timezone/data/latest.dart' as tz;
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  // await _configureLocalTimeZone();
   runApp(const MyApp());
 }
+
+// Future<void> _configureLocalTimeZone() async {
+//   tz.initializeTimeZones();
+//   final String timeZoneName = await FlutterNativeTimezone.getLocalTimezone();
+//   tz.setLocalLocation(tz.getLocation(timeZoneName));
+// }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flutter Notification Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Demo Notifications'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -54,69 +41,181 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+      FlutterLocalNotificationsPlugin();
+  bool _vibrationEnabled = true; // Pour gérer l'état de la vibration
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  @override
+  void initState() {
+    super.initState();
+    _initializeNotifications();
+  }
+
+  Future<void> _initializeNotifications() async {
+    // Initialisation pour Android
+    const AndroidInitializationSettings initializationSettingsAndroid =
+        AndroidInitializationSettings(
+          'ic_launcher',
+        ); // ic_launcher est le nom de l'icone de l'application
+
+    // Initialisation pour iOS
+    const DarwinInitializationSettings initializationSettingsIOS =
+        DarwinInitializationSettings(
+          requestAlertPermission: true,
+          requestBadgePermission: true,
+          requestSoundPermission: true,
+        );
+
+    // Paramètres d'initialisation globaux
+    const InitializationSettings initializationSettings =
+        InitializationSettings(
+          android: initializationSettingsAndroid,
+          iOS: initializationSettingsIOS,
+        );
+
+    // Initialise le plugin
+    await flutterLocalNotificationsPlugin.initialize(
+      initializationSettings,
+      onDidReceiveNotificationResponse: (
+        NotificationResponse notificationResponse,
+      ) {
+        // Gestion de la réponse à la notification (quand l'utilisateur clique dessus)
+        // Ici, on affiche un message dans la console pour l'exemple
+        debugPrint('Notification payload: ${notificationResponse.payload}');
+      },
+    );
+  }
+
+  // Fonction pour afficher une notification
+  Future<void> _showNotification({
+    required String title,
+    required String body,
+    String? payload,
+    bool playSound = true,
+    bool enableVibration = true,
+  }) async {
+    // Paramètres spécifiques à Android
+    AndroidNotificationDetails
+    androidNotificationDetails = AndroidNotificationDetails(
+      'your_channel_id', // Changer pour un ID de canal unique
+      'Your Channel Name', // Changer pour un nom de canal convivial
+      channelDescription:
+          'Description of your channel', // Changer pour une description du canal
+      importance: Importance.max,
+      priority: Priority.high,
+      playSound: playSound,
+      enableVibration: enableVibration,
+      //sound:
+      //    const RawResourceAndroidNotificationSound('sound'), // Décommenter et ajouter un fichier 'sound.wav' dans android/app/src/main/res/raw/
+      ticker: 'ticker',
+    );
+
+    // Paramètres spécifiques à iOS
+    DarwinNotificationDetails
+    iOSNotificationDetails = DarwinNotificationDetails(
+      presentAlert: true,
+      presentBadge: true,
+      presentSound: true,
+      sound:
+          'sound.wav', // Assurez-vous que le fichier son existe dans le projet.  Pour les sons personnalisés sur iOS, ils doivent être ajoutés au projet Xcode.
+    );
+
+    // Paramètres de notification globaux
+    NotificationDetails notificationDetails = NotificationDetails(
+      android: androidNotificationDetails,
+      iOS: iOSNotificationDetails,
+    );
+
+    // Affiche la notification
+    await flutterLocalNotificationsPlugin.show(
+      0, // ID de la notification (doit être unique)
+      title,
+      body,
+      notificationDetails,
+      payload:
+          payload, // Peut être utilisé pour passer des données à l'application quand l'utilisateur clique sur la notification
+    );
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+            ElevatedButton(
+              onPressed: () {
+                _showNotification(
+                  title: 'Notification Simple',
+                  body: 'Ceci est une notification Flutter !',
+                  payload: 'simple_notification_payload',
+                  playSound: false, // Vous pouvez changer cela
+                  enableVibration:
+                      _vibrationEnabled, // Utilise l'état de la vibration
+                );
+              },
+              child: const Padding(
+                padding: EdgeInsets.all(12.0),
+                child: Text(
+                  'Afficher Notification Sans Son',
+                  style: TextStyle(fontSize: 18),
+                ),
+              ),
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                _showNotification(
+                  title: 'Notification avec Son',
+                  body: 'Ceci est une notification avec un son personnalisé !',
+                  payload: 'sound_notification_payload',
+                  playSound: true,
+                  enableVibration: _vibrationEnabled,
+                );
+              },
+              child: const Padding(
+                padding: EdgeInsets.all(12.0),
+                child: Text(
+                  'Afficher Notification Avec Son',
+                  style: TextStyle(fontSize: 18),
+                ),
+              ),
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                setState(() {
+                  _vibrationEnabled =
+                      !_vibrationEnabled; // Inverse l'état de la vibration
+                });
+                // Affiche un message pour indiquer l'état de la vibration
+                ScaffoldMessenger.of(context).showSnackBar(
+                  SnackBar(
+                    content: Text(
+                      'Vibration ${_vibrationEnabled ? 'activée' : 'désactivée'}',
+                    ),
+                    duration: const Duration(seconds: 2),
+                  ),
+                );
+              },
+              child: Padding(
+                padding: const EdgeInsets.all(12.0),
+                child: Text(
+                  _vibrationEnabled
+                      ? 'Désactiver la Vibration'
+                      : 'Activer la Vibration',
+                  style: const TextStyle(fontSize: 18),
+                ),
+              ),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
